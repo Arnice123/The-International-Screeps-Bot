94ec9b5f04ff5bc9b7ecf09950b1eae33571ef61
Arnice123 <91489078+Arnice123@users.noreply.github.com>
2022-01-19 10:03:07 -0800

    finished making code for harv

--- src/creep/role.harvester.ts
+++ src/creep/role.harvester.ts
@@ -2,7 +2,34 @@
  This script handles the code for the harvester class
 */
 
+import { all, filter } from "lodash";
 
-/*
 
-*/
+var roleHarvester = {
+
+    run: function (creep: Creep) {
+
+        var sources = creep.room.find(FIND_SOURCES)
+
+        const closestSource = creep.pos.findClosestByRange(sources)
+
+        var containers = creep.room.find(FIND_MY_STRUCTURES, {
+            filter: { structureType: STRUCTURE_CONTAINER }
+        })
+
+        const closestContainer = creep.pos.findClosestByPath(containers)
+
+        if (!closestContainer) {
+            if (creep.harvest(closestSource) == ERR_NOT_IN_RANGE)
+                creep.moveTo(closestSource, { visualizePathStyle: { stroke: '#ffaa00' } })
+        }
+
+        if (creep.pos.getRangeTo(closestContainer.pos) == 0) {
+            creep.harvest(closestSource)
+            return
+        }
+
+        creep.moveTo(closestContainer, { visualizePathStyle: { stroke: '#ffaa00' } })
+
+    }
+};
