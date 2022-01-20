58d9b5ff56ff9edfb9f9d3ba4e9200fd2a94baee
Arnice123 <91489078+Arnice123@users.noreply.github.com>
2022-01-20 09:07:31 -0800

    organziation

--- src/creep/role.builder.ts
+++ src/creep/role.builder.ts
@@ -30,36 +30,38 @@ var roleBuilder = {
 
             const closestDroppedEnergy = creep.pos.findClosestByRange(droppedEnergy)
 
-            var containers = creep.room.find(FIND_MY_STRUCTURES, {
-                filter: { structureType: STRUCTURE_CONTAINER }
+            var containers: StructureContainer[] = creep.room.find(FIND_STRUCTURES, {
+                filter: (i) => i.structureType == STRUCTURE_CONTAINER &&
+                    i.store[RESOURCE_ENERGY] > 0
             })
 
-            const closestContainer = creep.pos.findClosestByPath(containers)
+            const closestContainer = creep.pos.findClosestByRange(containers)
 
-            const pathDropped = creep.pos.findPathTo(closestDroppedEnergy)
-            const pathContainer = creep.pos.findPathTo(closestContainer)
-
-            if (pathContainer.length > pathDropped.length) {
-                if (creep.pickup(closestDroppedEnergy) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(closestDroppedEnergy.pos);
-                }
+            if (closestDroppedEnergy.amount > closestContainer.store.energy) {
+                HarvestDropped(creep, closestDroppedEnergy)
             }
             else {
-                if (creep.withdraw(closestContainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(closestContainer);
-                }
-
-                if (creep.withdraw(closestContainer, RESOURCE_ENERGY) == ERR_NOT_ENOUGH_RESOURCES) {
-                    if (creep.pickup(closestDroppedEnergy) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(closestDroppedEnergy.pos);
-                    }
-                }
+                HarvestContainer(creep, closestContainer, closestDroppedEnergy)
             }
-
-
         }
     }
 }
 
 
+function HarvestContainer(creep: Creep, closestContainer: StructureContainer, closestDroppedEnergy: Resource<ResourceConstant>) {
+    if (creep.withdraw(closestContainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(closestContainer);
+    }
 
+    if (creep.withdraw(closestContainer, RESOURCE_ENERGY) == ERR_NOT_ENOUGH_RESOURCES) {
+        if (creep.pickup(closestDroppedEnergy) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(closestDroppedEnergy.pos);
+        }
+    }
+}
+
+function HarvestDropped(creep: Creep, energy: Resource<ResourceConstant>) {
+    if (creep.pickup(energy) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(energy.pos);
+    }
+}
